a18de21731b8f381225f1a6d730313447924afb6
Marcel Schramm <marceloschr@googlemail.com>
2024-12-24 13:27:07 +0100

    add opentablet driver

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -10,7 +10,8 @@
   environment.variables = {
     VDPAU_DRIVER = lib.mkIf config.hardware.opengl.enable (lib.mkDefault "va_gl");
   };
-  hardware.opengl.extraPackages = with pkgs; [
+  hardware.opentabletdriver.enable = true;
+  hardware.graphics.extraPackages = with pkgs; [
     (if (lib.versionOlder (lib.versions.majorMinor lib.version) "23.11") then vaapiIntel else intel-vaapi-driver)
     libvdpau-va-gl
     intel-media-driver
